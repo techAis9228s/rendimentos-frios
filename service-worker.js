d9f5da7f7469f7cbe80c5702f11ce58395e5508d
techAis9228s <estaca.cearabr@gmail.com>
2026-09-18 23:32:41 -0300

    v18: atualizar cache offline do PWA

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,47 @@
-const CACHE_NAME='rendimentos-frios-v17';
-const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE_NAME='rendimentos-frios-v18';
+const APP_SHELL=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon.svg',
+  './icon-192.png',
+  './icon-512.png'
+];
+
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+  );
   self.clients.claim();
 });
+
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
   const req=event.request;
+
   if(req.mode==='navigate'){
-    event.respondWith(fetch(req).then(res=>{
-      const copy=res.clone();
-      caches.open(CACHE_NAME).then(c=>c.put('./index.html',copy));
-      return res;
-    }).catch(()=>caches.match('./index.html').then(r=>r||caches.match('./'))));
+    event.respondWith(
+      fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE_NAME).then(c=>c.put('./index.html',copy));
+        return res;
+      }).catch(()=>caches.match('./index.html').then(r=>r||caches.match('./')))
+    );
     return;
   }
-  event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{
-    if(res&&res.status===200){
-      const copy=res.clone();
-      caches.open(CACHE_NAME).then(c=>c.put(req,copy));
-    }
-    return res;
-  })));
+
+  event.respondWith(
+    caches.match(req).then(cached=>cached||fetch(req).then(res=>{
+      if(res && res.status===200){
+        const copy=res.clone();
+        caches.open(CACHE_NAME).then(c=>c.put(req,copy));
+      }
+      return res;
+    }))
+  );
 });
