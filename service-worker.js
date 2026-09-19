e2fc046b2aea36fca89fbd150f4150b71287bb57
techAis9228s <estaca.cearabr@gmail.com>
2026-09-18 23:23:29 -0300

    Atualizar cache PWA para v17

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='rendimentos-frios-v15';
+const CACHE_NAME='rendimentos-frios-v17';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
